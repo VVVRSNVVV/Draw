b4fa7a4e5c01737bac31b2f8ed4b993034d32068
VVVRSNVVV <105973613+VVVRSNVVV@users.noreply.github.com>
2023-06-13 23:53:47 +0300

    portal

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private Transform destination;
+
+    public bool isOrange;
+    public float distance = 0.2f;
+
+     void Start()
+    {
+        if (isOrange == false)
+        {
+            destination = GameObject.FindGameObjectWithTag("orange portal").GetComponent<Transform>();
+
+        }
+        else
+        {
+            destination = GameObject.FindGameObjectWithTag("blue portal").GetComponent<Transform>();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Vector2.Distance(transform.position, other.transform.position)>distance) 
+        {
+            other.transform.position = new Vector2(destination.position.x, destination.position.y);
+        }
+    }
+
+
+
+
+}
