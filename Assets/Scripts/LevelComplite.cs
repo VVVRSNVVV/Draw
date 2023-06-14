ea8487a88465a2b32a3c6ee81bfc73c1c4151f07
VVVRSNVVV <105973613+VVVRSNVVV@users.noreply.github.com>
2023-06-14 18:09:12 +0300

    progressbar

---
+++ Assets/Scripts/LevelComplite.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelComplite : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] ScoreManager _scoreManager;
+    [SerializeField] private float maxValue = 100;
+
+
+    private void LateUpdate()
+    {
+        image.fillAmount = (float)_scoreManager.score / maxValue;
+    }
+
+
+
+}
