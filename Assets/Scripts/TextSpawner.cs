dad2de00bb56bdf6263a8ca2674d99fd40484ba9
VVVRSNVVV <slava.rusin2@gmail.com>
2023-06-20 21:30:15 +0300

    scenes

--- Assets/Scripts/TextSpawner.cs
+++ Assets/Scripts/TextSpawner.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+using DG.Tweening;
 public class TextSpawner : MonoBehaviour
 {
     [SerializeField] private TMP_Text labelPrefab;
-   
+    [SerializeField] private float animDuration;
+    [SerializeField] private float fullDuration;
+    [SerializeField] private Vector3 offset;
+
     public void Spawn(Vector3 position,string text)
           
     {
         var label = Instantiate(labelPrefab, position, Quaternion.identity);
         label.text = text;
-        Destroy(label.gameObject, 1.5f);
+        label.transform.DOMove(position + offset, animDuration);
+        label.transform.DOScale(Vector3.one, animDuration);
+        Destroy(label.gameObject, animDuration + fullDuration);
 
     }
     
