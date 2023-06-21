08daecb17829d7edca6224ea0b3423db01466fc2
VVVRSNVVV <slava.rusin2@gmail.com>
2023-06-21 19:47:22 +0300

    save

--- Assets/Scripts/buttons/MergeBalls.cs
+++ Assets/Scripts/buttons/MergeBalls.cs
@@ -1,13 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.WSA;
 
 public class MergeBalls : MonoBehaviour
 {
--- Assets/Scripts/buttons/NewBallON.cs
+++ Assets/Scripts/buttons/NewBallON.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.WSA;
 
 
 public class NewBallON : MonoBehaviour
