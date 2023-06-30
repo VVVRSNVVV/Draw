f06378484f1b55cbc161a31cb2d5573999561f8f
VVVRSNVVV <slava.rusin2@gmail.com>
2023-06-30 18:04:33 +0300

    Update RingScore.cs

--- Assets/Scripts/RingScore.cs
+++ Assets/Scripts/RingScore.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class RingScore : MonoBehaviour
 {
@@ -9,18 +6,14 @@ public class RingScore : MonoBehaviour
     private int score;
     [SerializeField] Transform textOrigin;
     [SerializeField] TextSpawner _textSpawner;
-   
+
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (true)
-        {
-            score = collision.GetComponent<BallScript>().score;
-            _scoreManager.scoring(score);
-            _textSpawner.Spawn(textOrigin.position, (score* ComboHandler.Instance.combo).ToString());
-            //ComboHandler.Instance.Add();
-        }
+        score = collision.GetComponent<BallScript>().score;
+        _scoreManager?.scoring(score);
+        _textSpawner.Spawn(textOrigin.position, (score* ComboHandler.Instance.combo).ToString());
     }
 
 
